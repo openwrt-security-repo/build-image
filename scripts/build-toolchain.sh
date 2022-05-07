96fa92f30dad5dc63e4836ebab74f7fd43e2c3d5
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2022-05-08 00:33:47 +0200

    fix deps

--- scripts/build-toolchain.sh
+++ scripts/build-toolchain.sh
@@ -7,8 +7,11 @@ cp -r /tmp/openwrt $BASE
 
 cd $BASE
 
-cp /tmp/board/config ./config
+cp /tmp/board/config ./.config
+echo "CONFIG_DEVEL=y" >> .config
 make defconfig
 
 make tools/install -j 32 V=s
 make toolchain/install -j 32 V=s
+
+make package/usign/compile V=sc -j32
