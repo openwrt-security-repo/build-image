c1ebc96411c27766af24cd0b62db10e179ae4c46
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2022-05-08 01:44:42 +0200

    Test build

--- scripts/test_env.sh
+++ scripts/test_env.sh
@@ -2,6 +2,16 @@
 
 ./scripts/feeds install nano
 
+cat <<EOF >> .config
+CONFIG_PACKAGE_libncurses=y
+CONFIG_PACKAGE_nano=y
+CONFIG_PACKAGE_nano-full=y
+CONFIG_PACKAGE_nano-plus=y
+CONFIG_PACKAGE_terminfo=y
+EOF
+
+make defconfig
+
 make package/nano/compile V=sc
 
 make package/index
